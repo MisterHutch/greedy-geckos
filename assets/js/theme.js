d891526b05c341c556705a7d18d00b20d12c4b84
hutch <83671235+MisterHutch@users.noreply.github.com>
2023-04-07 21:07:12 -0500

    greedyceckoz_hutch1v1 my updates

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,7 @@
 // Custom theme code
 
 if (document.getElementsByClassName('clean-gallery').length > 0) {
-   baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
+   baguetteBox.run('.clean-gallery', { animation: 'fade-out' });
 }
 
 if (document.getElementsByClassName('clean-product').length > 0) {
---
+++ matrix_visual.py
@@ -0,0 +1,26 @@
+import curses
+
+# initialize curses
+stdscr = curses.initscr()
+
+# turn off input echoing
+curses.noecho()
+
+# hide the cursor
+curses.curs_set(0)
+
+# enable color mode
+curses.start_color()
+
+# define color pairs
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+# set the green color for the binary code
+stdscr.attron(curses.color_pair(1))
+
+# ...
+
+# restore the terminal settings and exit curses
+curses.echo()
+curses.endwin()
