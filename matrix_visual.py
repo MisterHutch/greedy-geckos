38c30c77aec58deed55d9f1eb02051ad50d0c27e
hutch <83671235+MisterHutch@users.noreply.github.com>
2023-04-17 02:24:46 -0500

    trying to fix nav

--- matrix_visual.py
+++ matrix_visual.py
@@ -13,7 +13,7 @@ curses.curs_set(0)
 curses.start_color()
 
 # define color pairs
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_RED)
 curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
 # set the green color for the binary code
---
+++ script.js
@@ -0,0 +1,20 @@
+const textElement = document.querySelector('.text');
+
+textElement.addEventListener('click', () => {
+  const original = textElement.getAttribute('data-original');
+  const leet = textElement.getAttribute('data-leet');
+
+  if (textElement.textContent === original) {
+    textElement.style.transform = 'rotateY(180deg)';
+    setTimeout(() => {
+      textElement.textContent = leet;
+      textElement.style.transform = 'rotateY(360deg)';
+    }, 500);
+  } else {
+    textElement.style.transform = 'rotateY(180deg)';
+    setTimeout(() => {
+      textElement.textContent = original;
+      textElement.style.transform = 'rotateY(360deg)';
+    }, 500);
+  }
+});
